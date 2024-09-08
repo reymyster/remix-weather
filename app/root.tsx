80d638e1891587da1edbf92988a409cc1e1732fb
Rey Trajano <rey@reytrajano.com>
2024-09-08 00:27:33 -0700

    initial skeleton

--- app/root.tsx
+++ app/root.tsx
@@ -16,8 +16,21 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
-        {children}
+      <body className="h-[100svh] flex flex-col bg-slate-100">
+        <div className="flex-grow-0 h-16 flex items-center justify-between bg-slate-300/50 shadow-lg">
+          <h1 className="px-4 xl:px-6 text-2xl">Weather App</h1>
+          <div className="px-4 xl:px-6">
+            <input type="search" placeholder="Search cities..." className="p-2 rounded-md" />
+          </div>
+        </div>
+        <div className="flex-grow lg:w-[720px] my-2 mx-auto overflow-y-auto">
+          <div className="grid grid-cols-3 gap-2">
+            <div className="p-2 text-center">Rio</div>
+            <div className="p-2 text-center">Beijing</div>
+            <div className="p-2 text-center">Los Angeles</div>
+          </div>
+          {children}
+        </div>
         <ScrollRestoration />
         <Scripts />
       </body>
